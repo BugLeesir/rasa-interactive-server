5c5c2c5f9f0e17f3baae44da88e8c36787ebb88e
tj <813359062@qq.com>
2023-09-09 18:13:41 +0800

    前端界面加入水位流量关系线

--- rasa_project_charts/api.py
+++ rasa_project_charts/api.py
@@ -1,20 +1,21 @@
-from flask import Flask, jsonify
-import pandas as pd
+from flask import Flask, jsonify, send_file, request
 from knowledge_graph import graph_data
-
-
+from water_flow import get_water_flow_data
+import os
+import time
 app = Flask(__name__)
 
-data = pd.DataFrame({
-    "time": [1, 2, 3],
-    "water_level": [10, 15, 12],
-    "flow_rate": [5, 8, 6]
-})
 
-
-@app.route("/get_water_flow_data")
+@app.route("/get_water_flow_data", methods=["post"])
 def get_water_level_data():
-    return jsonify(data.to_dict(orient="records"))
+    base_path="./water_flow_charts"
+    place=request.json.get("place")
+    img_path=f"{base_path}/{place}.png"
+
+    if not os.path.exists(img_path):
+        get_water_flow_data(place)
+
+    return send_file(img_path,mimetype="image/png")
 
 
 @app.route("/get_graph_data", methods=["POST"])
@@ -23,5 +24,27 @@ def get_graph_data():
     return jsonify(graph_data_dict)
 
 
+# 每隔半个小时就会删除water_flow_charts文件夹下的所有文件
+def del_water_flow_charts():
+    folder_path="./water_flow_charts"
+
+    while True:
+        try:
+            for filename in os.listdir(folder_path):
+                file_path=os.path.join(folder_path,filename)
+                try:
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                except Exception as e:
+                    print(f"删除文件{file_path}时出错：{e}")
+
+            time.sleep(60*30)
+        except KeyboardInterrupt:
+            # 如果按下Ctrl+C，退出循环
+            break
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+    del_water_flow_charts()
